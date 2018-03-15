9c00e4e9c273aa1dac302af26e0784c6a4642d8a
Angel Mendez <angelmendezz751@gmail.com>
2018-03-15 14:28:58 -0600

    Al hacer Ctrl + s guardamos el documento en localStorage para algún futuro uso

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Editor, EditorState } from 'draft-js';
+import { Editor, EditorState, RichUtils, getDefaultKeyBinding, KeyBindingUtil, convertToRaw } from 'draft-js';
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { editorState: EditorState.createEmpty() };
-    this.onChange = (editorState) => this.setState({ editorState });
+    // this.onChange = (editorState) => this.setState({ editorState });
+    this.onChange = this.onChange.bind(this);
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+  }
+  keyBindingFn = (event) => {
+    // we press CTRL + K => return 'bbbold'
+    // we use hasCommandModifier instead of checking for CTRL keyCode because different OSs have different command keys
+    if (KeyBindingUtil.hasCommandModifier(event) && event.keyCode === 83) { return 'save'; }
+    // manages usual things, like:
+    // Ctrl+Z => return 'undo'
+    return getDefaultKeyBinding(event);
+  }
+
+  handleKeyCommand(command, editorState) {
+    let newState = RichUtils.handleKeyCommand(editorState, command);
+    const contentState = editorState.getCurrentContent();
+    if (command === 'save') {
+      this.saveContent(contentState);
+      // newState = RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD');
+    }
+
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  }
+  onChange = (editorState) => {
+    // const contentState = editorState.getCurrentContent();
+    // console.log('content state', convertToRaw(contentState));
+    // this.saveContent(contentState);
+    this.setState({
+      editorState,
+    })
+  }
+  saveContent = (content) => {
+    window.localStorage.setItem('content', JSON.stringify(convertToRaw(content)));
   }
   render() {
     return (
@@ -16,7 +52,12 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         Write down
-        <Editor editorState={this.state.editorState} onChange={this.onChange} />
+        <Editor
+          editorState={this.state.editorState}
+          onChange={this.onChange}
+          handleKeyCommand={this.handleKeyCommand}
+          keyBindingFn={this.keyBindingFn}
+        />
       </div>
     );
   }
