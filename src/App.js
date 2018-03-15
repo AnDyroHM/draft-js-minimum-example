5339a554a3e943544a9cd7ba73438c7dcefdc1d2
Angel Mendez <angelmendezz751@gmail.com>
2018-03-15 13:49:29 -0600

    Se ha puesto lo mínimo para que el editor funcione

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Editor, EditorState } from 'draft-js';
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { editorState: EditorState.createEmpty() };
+    this.onChange = (editorState) => this.setState({ editorState });
+  }
   render() {
     return (
       <div className="App">
@@ -10,9 +15,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          
-        </p>
+        Write down
+        <Editor editorState={this.state.editorState} onChange={this.onChange} />
       </div>
     );
   }
